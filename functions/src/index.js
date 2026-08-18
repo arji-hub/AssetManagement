1b7fc7c7df39151b735efe70db662f92976d5238
Glatterf <rggatmaitan23@gmail.com>
2026-08-18 14:05:27 +0800

    email transfer

--- functions/src/index.js
+++ functions/src/index.js
@@ -5,3 +5,7 @@ exports.onUpdateAssetCustodian =
   require("./triggers/onUpdateAssetCustodian").onUpdateAssetCustodian;
 exports.onUpdateAssetLocalMR =
   require("./triggers/onUpdateAssetLocalMR").onUpdateAssetLocalMR;
+exports.onTransferRequestCreated =
+  require("./triggers/onTransferRequestNotify").onTransferRequestCreated;
+exports.onTransferRequestUpdated =
+  require("./triggers/onTransferRequestNotify").onTransferRequestUpdated;
